d289043952fc18afab0f6e4d0b18afa2a78ea6b4
mibsambaig-web <mibsambaig@gmail.com>
2025-11-21 22:38:28 +0500

    Add files via upload

---
+++ 2hello.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    char name[50];
+
+    printf("Hey! What's your name? ");
+    scanf("%s", name);
+
+    printf("\n--- Birthday Message ---\n");
+
+    printf("\nHi %s!", name);
+    printf("\nThis message is specially made for Mina.\n");
+
+    printf("\nMina, you are really special.");
+    printf("\nYou brought happiness and peace into my life.");
+    printf("\nYou healed me in ways you don't even realize.");
+    printf("\nAnd I care about you so much.");
+    printf("\nMore than they I ever explain in words.\n");
+
+    printf("\nHappy Birthday, Mina! ❤");
+    printf("\nYou deserve all the love and happiness.\n");
+
+    return 0;
+}
